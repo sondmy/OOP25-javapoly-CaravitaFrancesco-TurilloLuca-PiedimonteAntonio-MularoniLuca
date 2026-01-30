adc7fabc37273c26f2ff7ca2cb15d2bdeff098e7
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-01-30 16:28:34 +0100

    added new type of Card (proprietyCard class)

---
+++ src/main/java/it/unibo/javapoly/model/impl/Card/ProprietyCard.java
@@ -0,0 +1,25 @@
+package it.unibo.javapoly.model.impl.Card;
+
+import it.unibo.javapoly.model.api.card.Card;
+
+public class ProprietyCard implements Card{
+
+    @Override
+    public String getId() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDescription'");
+    }
+    
+}
