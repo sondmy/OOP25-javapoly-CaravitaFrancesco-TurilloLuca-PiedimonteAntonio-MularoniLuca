f21bb8cc0db5eaf05578a382f7e10b5596244024
Luca Turillo <luca.turillo@studio.unibo.it>
2026-01-31 17:06:24 +0100

    add TokenImpl class to implement Token interface

---
+++ src/main/java/it/unibo/javapoly/model/player/TokenImpl.java
@@ -0,0 +1,22 @@
+package it.unibo.javapoly.model.player;
+
+// Visibilità package-private
+class TokenImpl implements Token {
+
+    private final String type;
+
+    // Costruttore Protected: solo la Factory può chiamarlo
+    protected TokenImpl(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Token[" + type + "]";
+    }
+}
