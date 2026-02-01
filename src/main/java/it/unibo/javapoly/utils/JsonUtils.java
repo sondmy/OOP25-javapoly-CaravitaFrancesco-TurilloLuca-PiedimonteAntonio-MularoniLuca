595cde88cd881c67f728a683ce5a5015410d16f4
Caravita Francesco <francesco.caravita2@studio.unibo.it>
2026-02-01 11:45:36 +0100

    added the RootName on mapper

--- src/main/java/it/unibo/javapoly/utils/JsonUtils.java
+++ src/main/java/it/unibo/javapoly/utils/JsonUtils.java
@@ -1,11 +1,11 @@
 package it.unibo.javapoly.utils;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-
-
+// TODO: add all the JavaDoc comment
 
 public final class JsonUtils {
     private static final ObjectMapper MAPPER = create();
@@ -18,6 +18,10 @@ public final class JsonUtils {
         m.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         m.enable(SerializationFeature.INDENT_OUTPUT); // per toString leggibile
         m.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+        // These two lines enable the @JsonRootName of the classes.
+        m.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        m.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
         return m;
     }
 
