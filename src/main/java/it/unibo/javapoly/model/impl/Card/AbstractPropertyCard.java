d1075fc6d524b3c7c6673b3a1c3470f45674266f
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-02 23:18:04 +0100

    added Error message and fixed checkstyle and pmd

--- src/main/java/it/unibo/javapoly/model/impl/Card/AbstractPropertyCard.java
+++ src/main/java/it/unibo/javapoly/model/impl/Card/AbstractPropertyCard.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import it.unibo.javapoly.model.api.RentContext;
 import it.unibo.javapoly.model.api.card.Card;
 import it.unibo.javapoly.utils.JsonUtils;
-
 
 /**
  * Base representation of a property card.
@@ -26,6 +26,21 @@ import it.unibo.javapoly.utils.JsonUtils;
 @JsonRootName("PropertyCard")
 public abstract class AbstractPropertyCard implements Card {
 
+    /**
+     * Error message used when a rent list is unexpectedly empty.
+     */
+    protected static final String ERR_LIST_IS_EMPTY = "The rent list is empty";
+
+    /**
+     * Error message used when a rent list is null.
+     */
+    protected static final String ERR_LIST_IS_NULL = "The rent list is null";
+
+    /**
+     * Error message used when an index is outside the valid bounds of a rent list.
+     */
+    protected static final String ERR_INDEX_OUT_LIMITS = "The given index is out of size: ";
+
     private final String id;
     private final String name;
     private final String description;
@@ -35,17 +50,17 @@ public abstract class AbstractPropertyCard implements Card {
     /**
      * Creates a new property card.
      *
-     * @param id the card identifier
-     * @param name the card name
-     * @param description the card description
-     * @param propertyCost the purchase cost of the property
-     * @param group the color / group of the property
+     * @param id the card identifier.
+     * @param name the card name.
+     * @param description the card description.
+     * @param propertyCost the purchase cost of the property.
+     * @param group the color / group of the property.
      */
     public AbstractPropertyCard(final String id,
-                         final String name,
-                         final String description,
-                         final int propertyCost,
-                         final String group) {
+                                final String name,
+                                final String description,
+                                final int propertyCost,
+                                final String group) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -54,9 +69,9 @@ public abstract class AbstractPropertyCard implements Card {
     }
 
     /**
-     * {@inheritDoc}
+     * Returns the unique identifier of the card.
      *
-     * @return the unique identifier of the card.
+     * @return the card identifier.
      */
     @Override
     public String getId() {
@@ -64,9 +79,9 @@ public abstract class AbstractPropertyCard implements Card {
     }
 
     /**
-     * {@inheritDoc}
+     * Returns the display name of the card.
      *
-     * @return the display name of the card.
+     * @return the display name.
      */
     @Override
     public String getName() {
@@ -74,9 +89,9 @@ public abstract class AbstractPropertyCard implements Card {
     }
 
     /**
-     * {@inheritDoc}
+     * Returns the description of the card.
      *
-     * @return the description of the card.
+     * @return the description.
      */
     @Override
     public String getDescription() {
@@ -106,17 +121,19 @@ public abstract class AbstractPropertyCard implements Card {
      * depending on the provided parameter (e.g. number of houses, or other
      * modifiers specific to the concrete property type).
      *
-     * @param number a parameter whose meaning depends on the concrete
-     *               implementation (for example: number of houses)
-     * @return the calculated rent for the given parameter.
+     * @param rent the rent calculation context (e.g. dice total, owned utilities).
+     * @return the calculated rent for the given context.
      */
-    public abstract int calculateRent(int number);
+    public abstract int calculateRent(RentContext rent);
 
     /**
      * Returns a JSON representation of this card.
      *
-     * @return the JSON string representing this card; if serialization
-     *         fails a short JSON error object is returned.
+     * <p>
+     * NOTE: this method uses {@code JsonUtils}. If you want to avoid this dependency
+     * in the model package, consider moving serialization logic elsewhere.
+     *
+     * @return the JSON string representing this card; if serialization fails a short JSON error object is returned.
      */
     @Override
     public String toString() {     // FIXME: Model can't use utils
