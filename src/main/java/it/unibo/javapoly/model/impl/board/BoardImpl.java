ccfc49189ffa1ded34a6f16fdba4a42cb75dbd45
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-04 19:11:56 +0100

    implemented the method

--- src/main/java/it/unibo/javapoly/model/impl/board/BoardImpl.java
+++ src/main/java/it/unibo/javapoly/model/impl/board/BoardImpl.java
@@ -1,26 +1,32 @@
 package it.unibo.javapoly.model.impl.board;
 
+import java.util.List;
+
 import it.unibo.javapoly.model.api.board.Board;
 import it.unibo.javapoly.model.api.board.Tile;
 
 public class BoardImpl implements Board{
 
+    private final List<Tile> tiles;
+
+    public BoardImpl(List<Tile> tiles) {
+        this.tiles = List.copyOf(tiles); 
+    }
+
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return tiles.size();
     }
 
     @Override
     public Tile getTileAt(int position) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTileAt'");
+        return tiles.get(normalizePosition(position));
     }
 
     @Override
     public int normalizePosition(int position) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'normalizePosition'");
+        int size = size();
+        return ((position % size) + size) % size;
     }
     
 }
