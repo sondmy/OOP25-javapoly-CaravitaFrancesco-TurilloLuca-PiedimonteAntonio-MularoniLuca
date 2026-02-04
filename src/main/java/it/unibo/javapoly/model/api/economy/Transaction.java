aa0fe1ffd715cb5023e6305acf759ed863cfdef0
Luca Mularoni <luca.mularoni@studio.unibo.it>
2026-02-05 00:15:33 +0100

    Change payer and payee to payerId and payeeId. Create static factory methods.

--- src/main/java/it/unibo/javapoly/model/api/economy/Transaction.java
+++ src/main/java/it/unibo/javapoly/model/api/economy/Transaction.java
@@ -1,7 +1,5 @@
 package it.unibo.javapoly.model.api.economy;
 
-import it.unibo.javapoly.model.api.Player;
-
 import java.util.Optional;
 
 /**
@@ -9,18 +7,69 @@ import java.util.Optional;
  *
  * @param id the unique identifier for this transaction.
  * @param type the type of transaction.
- * @param payer the player performing the payment (empty if bank).
- * @param payee the player receiving funds (empty if bank).
+ * @param payerId the unique identifier of the player performing the payment (empty if bank).
+ * @param payeeId the unique identifier of the player receiving funds (empty if bank).
  * @param propertyId the property involved in the transaction (empty if none).
  * @param amount the monetary amount of the transaction.
  */
 public record Transaction(
         int id,
         TransactionType type,
-        Optional<Player> payer,
-        Optional<Player> payee,
+        Optional<String> payerId,
+        Optional<String> payeeId,
         Optional<String> propertyId,
         int amount
 ) {
 
+    /**
+     * Creates a transaction for a player-to-player payment.
+     *
+     * @param id the unique identifier for this transaction.
+     * @param type the type of transaction.
+     * @param payerId the unique identifier of the player performing the payment (empty if bank).
+     * @param payeeId the unique identifier of the player receiving funds (empty if bank).
+     * @param amount the monetary amount of the transaction.
+     * @return a new Transaction
+     */
+    public static Transaction playerToPlayer(final int id,
+                                             final TransactionType type,
+                                             final String payerId,
+                                             final String payeeId,
+                                             final int amount) {
+        return new Transaction(id, type, Optional.of(payerId), Optional.of(payeeId), Optional.empty(), amount);
+
+    }
+
+    /**
+     * Creates a transaction for a bank-to-player payment.
+     *
+     * @param id the unique identifier for this transaction.
+     * @param type the type of transaction.
+     * @param payeeId the unique identifier of the player receiving funds (empty if bank).
+     * @param amount the monetary amount of the transaction.
+     * @return a new Transaction
+     */
+    public static Transaction bankToPlayer(final int id,
+                                             final TransactionType type,
+                                             final String payeeId,
+                                             final int amount) {
+        return new Transaction(id, type, Optional.empty(), Optional.of(payeeId), Optional.empty(), amount);
+    }
+
+    /**
+     * Creates a transaction for a player-to-bank payment.
+     *
+     * @param id the unique identifier for this transaction.
+     * @param type the type of transaction.
+     * @param payerId the unique identifier of the player performing the payment (empty if bank).
+     * @param amount the monetary amount of the transaction.
+     * @return a new Transaction
+     */
+    public static Transaction playerToBank(final int id,
+                                           final TransactionType type,
+                                           final String payerId,
+                                           final String propertyId,
+                                           final int amount) {
+        return new Transaction(id, type, Optional.of(payerId), Optional.empty(), Optional.of(propertyId), amount);
+    }
 }
