6ff6fbc910a57d39e1d71385de936942abe4350e
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-05 17:09:05 +0100

    finish method

--- src/main/java/it/unibo/javapoly/model/impl/Card/CardDeckImpl.java
+++ src/main/java/it/unibo/javapoly/model/impl/Card/CardDeckImpl.java
@@ -3,30 +3,59 @@ package it.unibo.javapoly.model.impl.card;
 import it.unibo.javapoly.model.api.card.CardDeck;
 import it.unibo.javapoly.model.api.card.GameCard;
 
-public class CardDeckImpl implements CardDeck{
+import java.util.*;
 
-    @Override
-    public GameCard draw(String playerId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+public class CardDeckImpl implements CardDeck {
+
+    private final Deque<GameCard> drawPile = new ArrayDeque<>();
+    private final Deque<GameCard> discardPile = new ArrayDeque<>();
+    private final Map<GameCard, String> heldCards = new HashMap<>();
+    private final Random random = new Random();
+
+    public CardDeckImpl(final List<GameCard> cards) {
+        this.drawPile.addAll(cards);
+        shuffle();
     }
 
     @Override
-    public void discard(GameCard card) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'discard'");
+    public GameCard draw(final String playerId) {
+        
+        if (drawPile.isEmpty()) {
+            recycleDiscard();
+        }
+
+        final GameCard card = drawPile.removeFirst();
+
+        if (card.isKeepUntilUsed()) {
+            heldCards.put(card, playerId);
+        }
+
+        return card;
+    }
+
+    @Override
+    public void discard(final GameCard card) {
+        heldCards.remove(card);
+        discardPile.addFirst(card);
     }
 
     @Override
     public void shuffle() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shuffle'");
+        final List<GameCard> temp = new ArrayList<>(drawPile);
+        Collections.shuffle(temp, random);
+        drawPile.clear();
+        drawPile.addAll(temp);
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+        return drawPile.isEmpty() && discardPile.isEmpty();
     }
-    
+
+    private void recycleDiscard() {
+        while (!discardPile.isEmpty()) {
+            drawPile.addLast(discardPile.removeFirst());
+        }
+        shuffle();
+    }
 }
