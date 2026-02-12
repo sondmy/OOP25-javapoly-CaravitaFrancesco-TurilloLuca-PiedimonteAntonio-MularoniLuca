926fb400ec4ebca380be3dbaa4770f753ff66ee9
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-12 23:25:39 +0100

    modified

--- src/main/java/it/unibo/javapoly/model/impl/property/PropertyImpl.java
+++ src/main/java/it/unibo/javapoly/model/impl/property/PropertyImpl.java
@@ -89,7 +89,7 @@ public final class PropertyImpl implements Property {
      */
     @Override
     public PropertyState getState() {
-        return new PropertyStateImpl(this.state);
+        return this.state;
     }
 
     /**
